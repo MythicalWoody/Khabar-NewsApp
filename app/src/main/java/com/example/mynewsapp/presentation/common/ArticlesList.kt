32108425eccef4c8d2bbc753792915d6c1773fce
MythicalWoody <shivamsingh.32p@gmail.com>
2024-02-26 04:57:30 +0530

    Bug fix for bookmark screen and error message screen

--- app/src/main/java/com/example/mynewsapp/presentation/common/ArticlesList.kt
+++ app/src/main/java/com/example/mynewsapp/presentation/common/ArticlesList.kt
@@ -20,6 +20,9 @@ fun ArticlesList(
     articles: List<Article>,
     onClick: (Article) -> Unit
 ) {
+    if (articles.isEmpty()){
+        EmptyScreen()
+    }
     LazyColumn(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(MediumPadding1),
@@ -74,6 +77,12 @@ fun handlePagingResult(
         }
 
         error != null -> {
+            EmptyScreen(
+                error = error
+            )
+            false
+        }
+        articles.itemCount == 0 -> {
             EmptyScreen()
             false
         }
